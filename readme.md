8ee1c29e4ea093cf83625fca7ca50acd2e193dcc
Pedro Ferreira <plferreira4@gmail.com>
2020-01-15 22:31:04 +0100

    Update the 'screen_ruler' to 'v2.2.0'.

--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 -   `brew install screen_ruler`
 -   `brew install tic_tac_toe`
 
+If an application fails to run due to lack of verification, you can allow it in: `System Preferences -> Security & Privacy -> General`.
+
 
 # To get the new sha256 hash
 
--- screen_ruler.rb
+++ screen_ruler.rb
@@ -1,19 +1,10 @@
-class ScreenRuler < Formula
-    include Language::Python::Virtualenv
+cask 'screen_ruler' do
+  version '2.2.0'
+  sha256 'c91bdcc4d26b13e2b4f3f835256bd8ba0c823ace3e1ad84a3a6a6c052d5bd07c'
 
-    desc "Screen ruler application"
-    homepage "https://github.com/noobiept/screen_ruler"
-    url "https://github.com/noobiept/screen_ruler/releases/download/v2.1.0/screen_ruler-2.1.0.zip"
-    sha256 "21da0288e4f876ed037bb6631e9704eaa8232cb3874088ae5ad1504cfe37568f"
+  url "https://github.com/noobiept/screen_ruler/releases/download/v2.2.0/screen_ruler.tar.gz"
+  name 'Screen Ruler'
+  homepage 'https://github.com/noobiept/screen_ruler'
 
-    depends_on "pyside"
-    depends_on "python"
-
-    def install
-      virtualenv_install_with_resources
-    end
-
-    test do
-      system "false"
-    end
-  end
+  app 'Screen Ruler.app'
+end
