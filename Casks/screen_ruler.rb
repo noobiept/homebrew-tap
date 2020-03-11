f192a38862075d3532dec7a7bf67d94c282bf7df
Pedro Ferreira <plferreira4@gmail.com>
2020-03-11 22:27:57 +0100

    Update the 'screen_ruler' shasum value for the 'v2.3.1' release.

--- Casks/screen_ruler.rb
+++ Casks/screen_ruler.rb
@@ -1,6 +1,6 @@
 cask 'screen_ruler' do
   version '2.3.1'
-  sha256 '59783c9ca30ecf754c5c99f3b4212520759daed60996f0d0335f3630a15288c9'
+  sha256 '477bac2f668ea75fd1facaf2502e2eb07d152c318fc173a312a7ed3e3e4f6dbf'
 
   url "https://github.com/noobiept/screen_ruler/releases/download/v2.3.1/screen_ruler.tar.gz"
   name 'Screen Ruler'
